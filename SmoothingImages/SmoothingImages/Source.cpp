c8a3df7a551a702fb7b542690fb67804535235a2
Jeremy J. Hartmann <jhartman@sfu.ca>
2016-10-06 21:31:50 -0400

    INIT: Repo to hold openCV experiments.

---
+++ SmoothingImages/SmoothingImages/Source.cpp
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// OPEN CV - SMOOTHING IMAGES TUTORIAL
+
+#include <iostream>
+
+
+
+
+
+using namespace std;
+
+int main() {
+
+    cout << "OpenCV Tutorial - Smoothing Images" << endl;
+
+
+
+
+
+    return 0;
+}

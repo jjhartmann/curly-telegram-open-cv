4a47ad1ddf85feab4634f4475fe8319ba365e1a8
Jeremy J. Hartmann <jhartman@sfu.ca>
2016-10-06 22:39:41 -0400

    Update test

--- SmoothingImages/SmoothingImages/Source.cpp
+++ SmoothingImages/SmoothingImages/Source.cpp
@@ -3,16 +3,32 @@
 
 #include <iostream>
 
-
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 
 
 using namespace std;
+using namespace cv;
 
-int main() {
+int main(int c, char** v) {
 
     cout << "OpenCV Tutorial - Smoothing Images" << endl;
 
+
+    Mat image = imread("1474605_10152119787690348_1306530646_n.jpg", IMREAD_COLOR);
+
+    if (!image.data)
+    {
+        cout << "Could not find images." << endl;
+        return -1;
+    }
+
+
+    namedWindow("Win1", WINDOW_AUTOSIZE);
+    imshow("Win1", image);
+
+    waitKey(0);
 
 
 
